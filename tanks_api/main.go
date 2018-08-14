58fd96401df3d25b4176d3e13a252d3e012a353d
barbarossa92 <donnietruth@gmail.com>
2018-08-14 16:42:47 +0600

    added new package maps && modified main.go

--- tanks_api/main.go
+++ tanks_api/main.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"log"
-	"math/rand"
 	"net/http"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/tanks_app/tanks_api/maps"
 )
 
-var clients = make(map[string]*websocket.Conn) // connected clients
-var broadcast = make(chan Message)             // broadcast channel
-var data = make(map[string]interface{})
+var broadcast = make(chan Message) // broadcast channel
 
 // Configure the upgrader
 var upgrader = websocket.Upgrader{
@@ -27,37 +24,7 @@ type Message struct {
 	Message  string `json:"message"`
 }
 
-type User struct {
-	name    string
-	coords  [2]int
-	murders int8
-	deaths  int8
-}
-
-type Map struct {
-	mapWidth  int
-	mapHeight int
-	schema    [][]interface{}
-	users     map[string]User
-	log       []string
-}
-
-var hashmap Map = Map{mapWidth: 20, mapHeight: 10, schema: [][]interface{}{}, users: make(map[string]User)}
-
-type Rocket struct {
-	tank string
-}
-
-type Tank struct {
-	route    string
-	name     string
-	tankType string
-}
-
-func (m *Map) writeToLog(message string) []string {
-	m.log = append(m.log, message)
-	return m.log
-}
+var hashmap = maps.Map{MapWidth: 20, MapHeight: 10, Schema: [][]interface{}{}, Users: make(map[string]maps.User), Clients: make(map[string]*websocket.Conn)}
 
 func main() {
 	// Create a simple file server
@@ -66,22 +33,20 @@ func main() {
 
 	// Configure websocket route
 	http.HandleFunc("/ws", handleConnections)
-	hashmap.schema = make([][]interface{}, hashmap.mapHeight)
-	for i := 0; i < hashmap.mapHeight; i++ {
-		hashmap.schema[i] = make([]interface{}, hashmap.mapWidth)
-		for j := 0; j < hashmap.mapWidth; j++ {
-			hashmap.schema[i][j] = "null"
+	hashmap.Schema = make([][]interface{}, hashmap.MapHeight)
+	for i := 0; i < hashmap.MapHeight; i++ {
+		hashmap.Schema[i] = make([]interface{}, hashmap.MapWidth)
+		for j := 0; j < hashmap.MapWidth; j++ {
+			hashmap.Schema[i][j] = "null"
 		}
 	}
-	hashmap.schema[1][1] = "wall"
-	hashmap.schema[1][2] = "wall"
-	hashmap.schema[1][3] = "wall"
-	hashmap.schema[2][1] = "wall"
-	hashmap.schema[7][2] = "wall"
-	hashmap.schema[7][3] = "wall"
-	hashmap.schema[7][4] = "wall"
-	data["map"] = hashmap.schema
-	data["log"] = hashmap.log
+	hashmap.Schema[1][1] = "wall"
+	hashmap.Schema[1][2] = "wall"
+	hashmap.Schema[1][3] = "wall"
+	hashmap.Schema[2][1] = "wall"
+	hashmap.Schema[7][2] = "wall"
+	hashmap.Schema[7][3] = "wall"
+	hashmap.Schema[7][4] = "wall"
 	var mutex sync.Mutex
 	// SetMaps()
 	// Start listening for incoming chat messages
@@ -106,10 +71,11 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 	defer ws.Close()
 	// Register our new client
 	if ok {
-		clients[username[0]] = ws
+		hashmap.Clients[username[0]] = ws
 	}
-	data["map"] = hashmap.schema
-	data["log"] = hashmap.log
+	data := make(map[string]interface{})
+	data["map"] = hashmap.Schema
+	data["log"] = hashmap.Log
 	ws.WriteJSON(data)
 	for {
 		var msg Message
@@ -117,22 +83,12 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 		err := ws.ReadJSON(&msg)
 		if err != nil {
 			log.Printf("error: %v", err)
-			delete(clients, username[0])
+			delete(hashmap.Clients, username[0])
 			break
 		}
 		// Send the newly received message to the broadcast channel
 		broadcast <- msg
 	}
-}
-
-func findNullRect(m *Map) [2]int {
-	randWidth := random(0, m.mapWidth)
-	randHeigth := random(0, m.mapHeight)
-	if m.schema[randHeigth][randWidth] == "null" {
-		coords := [2]int{randHeigth, randWidth}
-		return coords
-	}
-	return findNullRect(m)
 }
 
 func handleMessages(mutex *sync.Mutex) {
@@ -142,209 +98,23 @@ func handleMessages(mutex *sync.Mutex) {
 		log.Printf("%v", msg)
 		username := msg.Username
 		command := msg.Message
+		data := make(map[string]interface{})
+		data["map"] = hashmap.Schema
 		if command == "create" {
-			CreateTank(username)
-			log := hashmap.writeToLog(strings.Split(username, "-")[0] + " вошел в игру.")
+			hashmap.CreateTank(username)
+			log := hashmap.WriteToLog(strings.Split(username, "-")[0] + " вошел в игру.")
 			data["log"] = log
-			sendToClients(mutex, data)
+			hashmap.SendToClients(mutex, data)
 		} else if command == "up" || command == "down" || command == "right" || command == "left" {
-			StepUser(username, command)
-			sendToClients(mutex, data)
+			hashmap.StepUser(username, command)
+			hashmap.SendToClients(mutex, data)
 		} else if command == "delete" {
-			DeleteTank(username)
-			log := hashmap.writeToLog(strings.Split(username, "-")[0] + " вышел из игры.")
+			hashmap.DeleteTank(username)
+			log := hashmap.WriteToLog(strings.Split(username, "-")[0] + " вышел из игры.")
 			data["log"] = log
-			sendToClients(mutex, data)
+			hashmap.SendToClients(mutex, data)
 		} else if command == "fire" {
-			go rocketFire(username, mutex)
+			go hashmap.RocketFire(username, mutex)
 		}
 	}
 }
-
-func StepUser(username, route string) {
-	user := hashmap.users[username]
-	coords := user.coords
-	tank := hashmap.schema[coords[0]][coords[1]]
-	if _, ok := tank.(map[string]interface{}); ok {
-		if tank.(map[string]interface{})["route"] == route {
-			if route == "up" && coords[0] > 0 && hashmap.schema[coords[0]-1][coords[1]] == "null" {
-				tank.(map[string]interface{})["coords"] = [2]int{coords[0] - 1, coords[1]}
-				hashmap.schema[coords[0]-1][coords[1]] = tank
-				hashmap.schema[coords[0]][coords[1]] = "null"
-				user.coords[0]--
-				hashmap.users[username] = user
-			} else if route == "down" && coords[0] < hashmap.mapHeight-1 && hashmap.schema[coords[0]+1][coords[1]] == "null" {
-				tank.(map[string]interface{})["coords"] = [2]int{coords[0] + 1, coords[1]}
-				hashmap.schema[coords[0]+1][coords[1]] = tank
-				hashmap.schema[coords[0]][coords[1]] = "null"
-				user.coords[0]++
-				hashmap.users[username] = user
-			} else if route == "right" && coords[1] < hashmap.mapWidth-1 && hashmap.schema[coords[0]][coords[1]+1] == "null" {
-				tank.(map[string]interface{})["coords"] = [2]int{coords[0], coords[1] + 1}
-				hashmap.schema[coords[0]][coords[1]+1] = tank
-				hashmap.schema[coords[0]][coords[1]] = "null"
-				user.coords[1]++
-				hashmap.users[username] = user
-			} else if route == "left" && coords[1] > 0 && hashmap.schema[coords[0]][coords[1]-1] == "null" {
-				tank.(map[string]interface{})["coords"] = [2]int{coords[0], coords[1] - 1}
-				hashmap.schema[coords[0]][coords[1]-1] = tank
-				hashmap.schema[coords[0]][coords[1]] = "null"
-				user.coords[1]--
-				hashmap.users[username] = user
-			} else {
-				return
-			}
-		} else {
-			hashmap.schema[coords[0]][coords[1]].(map[string]interface{})["route"] = route
-		}
-	}
-	return
-}
-
-func CreateTank(username string) {
-	coords := findNullRect(&hashmap)
-	tank := Tank{route: "right", name: username, tankType: "user"}
-	tankMap := make(map[string]interface{})
-	tankMap["route"] = tank.route
-	tankMap["name"] = tank.name
-	tankMap["tankType"] = tank.tankType
-	tankMap["coords"] = coords
-	hashmap.schema[coords[0]][coords[1]] = tankMap
-	hashmap.users[tank.name] = User{name: tank.name, coords: coords, murders: 0, deaths: 0}
-}
-
-func DeleteTank(username string) {
-	var user User = hashmap.users[username]
-	coords := user.coords
-	hashmap.schema[coords[0]][coords[1]] = "null"
-	delete(hashmap.users, username)
-}
-
-func checkRect(rect interface{}) bool {
-	switch rect {
-	case "wall", "tank", "dummy":
-		return false
-	}
-	return true
-}
-
-func random(min, max int) int {
-	rand.Seed(time.Now().Unix())
-	return rand.Intn(max-min) + min
-}
-
-func sendToClients(mutex *sync.Mutex, data interface{}) {
-	// Send it out to every client that is currently connected
-	mutex.Lock()
-	for client := range clients {
-		err := clients[client].WriteJSON(data)
-		if err != nil {
-			log.Printf("error: %v", err)
-			clients[client].Close()
-			delete(clients, client)
-		}
-	}
-	mutex.Unlock()
-}
-
-func rocketFire(username string, mutex *sync.Mutex) {
-	user, ok := hashmap.users[username]
-	if ok {
-		coords := user.coords
-		tank := hashmap.schema[coords[0]][coords[1]]
-		rocket := Rocket{tank: username}
-		rocketMap := map[string]interface{}{"tank": rocket.tank}
-		var arrLevel, indexHeight, indexWidth, tmpVal int
-		var equalVal bool
-		hasRoute := false
-		route := tank.(map[string]interface{})["route"]
-		for {
-			switch {
-			case route == "up":
-				arrLevel = 0
-				indexHeight = coords[0] - 1
-				indexWidth = coords[1]
-				equalVal = coords[0]-1 >= 0
-				hasRoute = true
-				tmpVal = -1
-			case route == "down":
-				arrLevel = 0
-				indexHeight = coords[0] + 1
-				indexWidth = coords[1]
-				equalVal = coords[0]+1 <= hashmap.mapHeight-1
-				hasRoute = true
-				tmpVal = 1
-			case route == "left":
-				arrLevel = 1
-				indexHeight = coords[0]
-				indexWidth = coords[1] - 1
-				equalVal = coords[1]-1 >= 0
-				hasRoute = true
-				tmpVal = -1
-			case route == "right":
-				arrLevel = 1
-				indexHeight = coords[0]
-				indexWidth = coords[1] + 1
-				equalVal = coords[1]+1 <= hashmap.mapWidth-1
-				hasRoute = true
-				tmpVal = 1
-			}
-			if hasRoute {
-				if equalVal {
-					nextRect := hashmap.schema[indexHeight][indexWidth]
-					if nextRect != "null" {
-						if nextRect == "wall" {
-							if _, ok := hashmap.schema[coords[0]][coords[1]].(map[string]interface{})["tank"]; ok {
-								hashmap.schema[coords[0]][coords[1]] = "null"
-								sendToClients(mutex, data)
-							}
-							break
-						} else if _, ok := nextRect.(map[string]interface{})["route"]; ok {
-							victimName := hashmap.schema[indexHeight][indexWidth].(map[string]interface{})["name"]
-							hashmap.schema[indexHeight][indexWidth] = "null"
-							if _, ok := hashmap.schema[coords[0]][coords[1]].(map[string]interface{})["tank"]; ok {
-								hashmap.schema[coords[0]][coords[1]] = "null"
-							}
-							victim := hashmap.users[victimName.(string)]
-							victim.deaths++
-							user.murders++
-							hashmap.users[victimName.(string)] = victim
-							hashmap.users[username] = user
-							log := hashmap.writeToLog(strings.Split(username, "-")[0] + " убил " + strings.Split(victimName.(string), "-")[0])
-							data["log"] = log
-							sendToClients(mutex, data)
-							clients[victimName.(string)].WriteJSON(map[string]bool{"dead": true})
-							break
-						}
-					} else {
-						if _, ok := hashmap.schema[coords[0]][coords[1]].(map[string]interface{})["tank"]; ok {
-							hashmap.schema[coords[0]][coords[1]] = "null"
-						}
-						hashmap.schema[indexHeight][indexWidth] = rocketMap
-						coords[arrLevel] = coords[arrLevel] + tmpVal
-					}
-				} else {
-					if _, ok := hashmap.schema[coords[0]][coords[1]].(map[string]interface{})["tank"]; ok {
-						hashmap.schema[coords[0]][coords[1]] = "null"
-						sendToClients(mutex, data)
-					}
-					break
-				}
-			}
-			sendToClients(mutex, data)
-			time.Sleep(30 * time.Millisecond)
-		}
-		return
-	} else {
-		log.Printf("User %v is not found!", username)
-		return
-	}
-}
-
-// func BarbarossaBot() {
-// 	coords := findNullRect(&hashmap)
-// 	hashmap.users["BarbarossaBot"] = User{name: "BarbarossaBot", coords: coords, murders: 0, deaths: 0}
-// 	for {
-
-// 	}
-// }
---
+++ tanks_api/maps/maps.go
@@ -0,0 +1,253 @@
+package maps
+
+import (
+	"log"
+	"math/rand"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+type User struct {
+	Name    string
+	Coords  [2]int
+	Murders int
+	Deaths  int
+}
+
+type Map struct {
+	MapWidth  int
+	MapHeight int
+	Schema    [][]interface{}
+	Users     map[string]User
+	Log       []string
+	Clients   map[string]*websocket.Conn
+}
+
+type Rocket struct {
+	Tank string
+}
+
+type Tank struct {
+	Route    string
+	Name     string
+	TankType string
+}
+
+func (m *Map) WriteToLog(message string) []string {
+	m.Log = append(m.Log, message)
+	return m.Log
+}
+
+func (m *Map) FindNullRect() [2]int {
+	randWidth := random(0, m.MapWidth)
+	randHeigth := random(0, m.MapHeight)
+	if m.Schema[randHeigth][randWidth] == "null" {
+		coords := [2]int{randHeigth, randWidth}
+		return coords
+	}
+	return m.FindNullRect()
+}
+
+func (m *Map) RatingRefresh() map[int][2]interface{} {
+	rating := make(map[int][2]interface{}, 3)
+	i := 1
+	for {
+		if i > 3 {
+			break
+		} else {
+			tmpMax := 0
+			var tmpUser [2]interface{}
+			for u, k := range m.Users {
+				if k.Murders > tmpMax {
+					tmpMax = k.Murders
+					tmpUser[0] = u
+					tmpUser[1] = k.Murders
+				}
+			}
+			rating[i] = tmpUser
+			i++
+		}
+
+	}
+	return rating
+}
+
+func (m *Map) StepUser(username, route string) {
+	user := m.Users[username]
+	coords := user.Coords
+	tank := m.Schema[coords[0]][coords[1]]
+	if _, ok := tank.(map[string]interface{}); ok {
+		if tank.(map[string]interface{})["route"] == route {
+			if route == "up" && coords[0] > 0 && m.Schema[coords[0]-1][coords[1]] == "null" {
+				tank.(map[string]interface{})["coords"] = [2]int{coords[0] - 1, coords[1]}
+				m.Schema[coords[0]-1][coords[1]] = tank
+				m.Schema[coords[0]][coords[1]] = "null"
+				user.Coords[0]--
+				m.Users[username] = user
+			} else if route == "down" && coords[0] < m.MapHeight-1 && m.Schema[coords[0]+1][coords[1]] == "null" {
+				tank.(map[string]interface{})["coords"] = [2]int{coords[0] + 1, coords[1]}
+				m.Schema[coords[0]+1][coords[1]] = tank
+				m.Schema[coords[0]][coords[1]] = "null"
+				user.Coords[0]++
+				m.Users[username] = user
+			} else if route == "right" && coords[1] < m.MapWidth-1 && m.Schema[coords[0]][coords[1]+1] == "null" {
+				tank.(map[string]interface{})["coords"] = [2]int{coords[0], coords[1] + 1}
+				m.Schema[coords[0]][coords[1]+1] = tank
+				m.Schema[coords[0]][coords[1]] = "null"
+				user.Coords[1]++
+				m.Users[username] = user
+			} else if route == "left" && coords[1] > 0 && m.Schema[coords[0]][coords[1]-1] == "null" {
+				tank.(map[string]interface{})["coords"] = [2]int{coords[0], coords[1] - 1}
+				m.Schema[coords[0]][coords[1]-1] = tank
+				m.Schema[coords[0]][coords[1]] = "null"
+				user.Coords[1]--
+				m.Users[username] = user
+			} else {
+				return
+			}
+		} else {
+			m.Schema[coords[0]][coords[1]].(map[string]interface{})["route"] = route
+		}
+	}
+	return
+}
+
+func (m *Map) CreateTank(username string) {
+	coords := m.FindNullRect()
+	tank := Tank{Route: "right", Name: username, TankType: "user"}
+	tankMap := make(map[string]interface{})
+	tankMap["route"] = tank.Route
+	tankMap["name"] = tank.Name
+	tankMap["tankType"] = tank.TankType
+	tankMap["coords"] = coords
+	m.Schema[coords[0]][coords[1]] = tankMap
+	m.Users[tank.Name] = User{Name: tank.Name, Coords: coords, Murders: 0, Deaths: 0}
+}
+
+func (m *Map) DeleteTank(username string) {
+	var user User = m.Users[username]
+	coords := user.Coords
+	m.Schema[coords[0]][coords[1]] = "null"
+	delete(m.Users, username)
+}
+
+func random(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
+}
+
+func (m *Map) SendToClients(mutex *sync.Mutex, data map[string]interface{}) {
+	// Send it out to every client that is currently connected
+	mutex.Lock()
+	for client := range m.Clients {
+		err := m.Clients[client].WriteJSON(data)
+		if err != nil {
+			log.Printf("error: %v", err)
+			m.Clients[client].Close()
+			delete(m.Clients, client)
+		}
+	}
+	mutex.Unlock()
+}
+
+func (m *Map) RocketFire(username string, mutex *sync.Mutex) {
+	user, ok := m.Users[username]
+	if ok {
+		data := make(map[string]interface{})
+		data["map"] = m.Schema
+		coords := user.Coords
+		tank := m.Schema[coords[0]][coords[1]]
+		rocket := Rocket{Tank: username}
+		rocketMap := map[string]interface{}{"tank": rocket.Tank}
+		var arrLevel, indexHeight, indexWidth, tmpVal int
+		var equalVal bool
+		hasRoute := false
+		route := tank.(map[string]interface{})["route"]
+		for {
+			switch {
+			case route == "up":
+				arrLevel = 0
+				indexHeight = coords[0] - 1
+				indexWidth = coords[1]
+				equalVal = coords[0]-1 >= 0
+				hasRoute = true
+				tmpVal = -1
+			case route == "down":
+				arrLevel = 0
+				indexHeight = coords[0] + 1
+				indexWidth = coords[1]
+				equalVal = coords[0]+1 <= m.MapHeight-1
+				hasRoute = true
+				tmpVal = 1
+			case route == "left":
+				arrLevel = 1
+				indexHeight = coords[0]
+				indexWidth = coords[1] - 1
+				equalVal = coords[1]-1 >= 0
+				hasRoute = true
+				tmpVal = -1
+			case route == "right":
+				arrLevel = 1
+				indexHeight = coords[0]
+				indexWidth = coords[1] + 1
+				equalVal = coords[1]+1 <= m.MapWidth-1
+				hasRoute = true
+				tmpVal = 1
+			}
+			if hasRoute {
+				if equalVal {
+					nextRect := m.Schema[indexHeight][indexWidth]
+					if nextRect != "null" {
+						if nextRect == "wall" {
+							if _, ok := m.Schema[coords[0]][coords[1]].(map[string]interface{})["tank"]; ok {
+								m.Schema[coords[0]][coords[1]] = "null"
+								m.SendToClients(mutex, data)
+							}
+							break
+						} else if _, ok := nextRect.(map[string]interface{})["route"]; ok {
+							victimName := m.Schema[indexHeight][indexWidth].(map[string]interface{})["name"]
+							m.Schema[indexHeight][indexWidth] = "null"
+							if _, ok := m.Schema[coords[0]][coords[1]].(map[string]interface{})["tank"]; ok {
+								m.Schema[coords[0]][coords[1]] = "null"
+							}
+							victim := m.Users[victimName.(string)]
+							victim.Deaths++
+							user.Murders++
+							m.Users[victimName.(string)] = victim
+							m.Users[username] = user
+							logs := m.WriteToLog(strings.Split(username, "-")[0] + " убил " + strings.Split(victimName.(string), "-")[0])
+							data["log"] = logs
+							data["rating"] = m.RatingRefresh()
+							m.SendToClients(mutex, data)
+							if conn, ok := m.Clients[victimName.(string)]; ok {
+								conn.WriteJSON(map[string]bool{"dead": true})
+							}
+							break
+						}
+					} else {
+						if _, ok := m.Schema[coords[0]][coords[1]].(map[string]interface{})["tank"]; ok {
+							m.Schema[coords[0]][coords[1]] = "null"
+						}
+						m.Schema[indexHeight][indexWidth] = rocketMap
+						coords[arrLevel] = coords[arrLevel] + tmpVal
+					}
+				} else {
+					if _, ok := m.Schema[coords[0]][coords[1]].(map[string]interface{})["tank"]; ok {
+						m.Schema[coords[0]][coords[1]] = "null"
+						m.SendToClients(mutex, data)
+					}
+					break
+				}
+			}
+			m.SendToClients(mutex, data)
+			time.Sleep(30 * time.Millisecond)
+		}
+		return
+	} else {
+		log.Printf("User %v is not found!", username)
+		return
+	}
+}
